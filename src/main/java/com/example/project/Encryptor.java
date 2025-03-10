0de419fdd2298e863389dcbeabfb5b39bfa0a607
Pako-12317 <patrykm10@nycstudents.net>
2025-03-10 21:15:09 +0000

    ,commit

--- src/main/java/com/example/project/Encryptor.java
+++ src/main/java/com/example/project/Encryptor.java
@@ -17,14 +17,64 @@ public class Encryptor {
     }
     
     public static String[][] generateEncryptArray(String message, int rows) {
-        return new String [rows][determineColumns(message.length(), rows)];
+        String[][] messageArray = new String [rows][determineColumns(message.length(), rows)];
+        int index = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < determineColumns(message.length(), rows); c++) {
+                if (index < message.length()) {
+                    messageArray[r][c] = message.charAt(index++) + "";
+                } else {
+                    messageArray[r][c] = "=";
+                }
+            }
+        }
+
+        return messageArray;
     }
 
-    public static String encryptMessage(String message, int rows){
-        return "";
+    public static String encryptMessage(String message, int rows) {
+        int len = message.length();
+        int col = determineColumns(len, rows);
+        String[][] messageList = generateEncryptArray(message, rows);
+
+
+        String encryptedMessage = "";
+        for (int i = col - 1; i >= 0; i--) {
+            for (int j = 0; j < rows; j++) {
+                encryptedMessage += messageList[j][i];
+            }
+        }
+
+
+        return encryptedMessage;
     }
 
     public static String decryptMessage(String encryptedMessage, int rows) {
-        return "";
+        int len = encryptedMessage.length();
+        int col = determineColumns(len, rows);
+        String[][] messageArray = new String[rows][col];
+        int index = 0;
+
+        for (int i = col - 1; i >= 0; i--) {
+            for (int j = 0; j < rows; j++) {
+                if (index < len) {
+                    messageArray[j][i] = encryptedMessage.charAt(index++) + "";
+                } else {
+                    messageArray[j][i] = "=";
+                }
+            }
+        }
+
+        String decryptedMessage = "";
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < col; c++) {
+                if (!messageArray[r][c].equals("=")) {
+                    decryptedMessage += messageArray[r][c];
+                }
+            }
+        }
+
+        return decryptedMessage;
     }
 }
