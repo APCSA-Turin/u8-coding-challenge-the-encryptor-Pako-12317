8a38283699294b45257be5d0bcb7707a35fd13d8
Pako-12317 <patrykm10@nycstudents.net>
2025-03-10 13:25:24 +0000

    ,commit

--- src/main/java/com/example/project/Encryptor.java
+++ src/main/java/com/example/project/Encryptor.java
@@ -5,12 +5,19 @@ import java.util.Arrays;
 public class Encryptor {
     
     public static int determineColumns(int messageLen, int rows){
-        
-        return 0;
+        if (messageLen == 0) {
+            return 1;
+        }else{
+            int i = 0;
+            while(rows*i<messageLen){
+                i++;
+            }
+            return i;
+        }
     }
     
     public static String[][] generateEncryptArray(String message, int rows) {
-        return new String [0][0];
+        return new String [rows][determineColumns(message.length(), rows)];
     }
 
     public static String encryptMessage(String message, int rows){
